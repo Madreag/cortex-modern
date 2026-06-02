91e9aef7cea89bf730e45f1da3b3d28ab130fcd2
Erol Germain-Gomuc <egermain.ua@gmail.com>
2026-06-01 22:52:18 -0700

    Draw cosmetic effects from the render RNG, not the sim stream
    Muzzle flashes, glow post-effects and screen shake drew from the free RandomNum()
    (the sim RNG) gated on render state, advancing the sim RNG a host-dependent number
    of times. Route them to g_RenderRNG by name as the sim/render split intends.

--- Source/Managers/PostProcessMan.cpp
+++ Source/Managers/PostProcessMan.cpp
@@ -389,7 +389,7 @@ void PostProcessMan::DrawDotGlowEffects() {
 				testpixel = _getpixel(g_FrameMan.GetBackBuffer8(), x, y);
 
 				// YELLOW
-				if ((testpixel == g_YellowGlowColor && RandomNum() < 0.9F) || testpixel == 98 || (testpixel == 120 && RandomNum() < 0.7F)) {
+				if ((testpixel == g_YellowGlowColor && g_RenderRNG.RandomNum() < 0.9F) || testpixel == 98 || (testpixel == 120 && g_RenderRNG.RandomNum() < 0.7F)) {
 					DrawTexture(yellowGlow, x - yellowGlow.width / 2, y - yellowGlow.height / 2, {255, 255, 255, 255});
 				}
 				// TODO: Enable and add more colors once we actually have something that needs these.
