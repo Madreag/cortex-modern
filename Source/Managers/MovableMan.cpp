ceffd131fa5f196a097706cc270d82fd5a1e5c1f
Erol Germain-Gomuc <egermain.ua@gmail.com>
2026-09-06 04:30:47 -0700

    Dump the value a script's Create found

--- Source/Managers/MovableMan.cpp
+++ Source/Managers/MovableMan.cpp
@@ -744,7 +744,8 @@ void MovableMan::DumpSimState(uint64_t tick, std::ostream& out) const {
 				LuaStateWrapper* state = actor->GetLuaState();
 				const long create = state ? static_cast<long>(state->GetScriptObjectNumberField(actor->GetUniqueID(), "testCreate", -1.0)) : -1;
 				const long update = state ? static_cast<long>(state->GetScriptObjectNumberField(actor->GetUniqueID(), "testUpdate", -1.0)) : -1;
-				out << " script=" << create << "/" << update << "/" << static_cast<long>(actor->GetNumberValue("TestUpdates"));
+				const long carried = state ? static_cast<long>(state->GetScriptObjectNumberField(actor->GetUniqueID(), "testCarried", -1.0)) : -1;
+				out << " script=" << create << "/" << update << "/" << static_cast<long>(actor->GetNumberValue("TestUpdates")) << "/" << carried;
 			}
 			if (const ACraft* craft = dynamic_cast<const ACraft*>(mo)) {
 				out << " hatch=" << static_cast<int>(craft->GetHatchState()) << " deathms=" << craft->GetDeathTimerElapsedSimMS();
