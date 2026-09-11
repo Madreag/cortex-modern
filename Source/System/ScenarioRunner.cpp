a902adabd04dceba2c659cf29c0d49172bbbd125
Erol Germain-Gomuc <egermain.ua@gmail.com>
2026-09-11 06:32:29 -0700

    Keep the lockstep coordinator alive across a match-over rejoin

--- Source/System/ScenarioRunner.cpp
+++ Source/System/ScenarioRunner.cpp
@@ -1586,6 +1586,10 @@ namespace RTE {
 					s_SessionPump();
 				}
 			}
+			if (!s_LockstepCoordinator) {
+				if (error) *error = "lockstep coordinator is not active";
+				return false;
+			}
 			NetLockstepReadyFrame ready;
 			while (s_LockstepCoordinator->PopReadyFrame(ready)) {
 				if (ready.frame == tick) {
