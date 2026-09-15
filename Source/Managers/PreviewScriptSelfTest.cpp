cf8f38a660a54e053f12988f428d5221b1142503
Erol Germain-Gomuc <egermain.ua@gmail.com>
2026-09-15 10:03:52 -0700

    Compile the leftover-abort probe without the C abort macro

--- Source/Managers/PreviewScriptSelfTest.cpp
+++ Source/Managers/PreviewScriptSelfTest.cpp
@@ -30,7 +30,6 @@ extern "C" {
 #include "lj_obj.h"
 #include "lj_jit.h"
 #include "lj_dispatch.h"
-#include "lj_trace.h"
 }
 
 #include <iostream>
@@ -396,7 +395,7 @@ assert(seen > 0, 'no trace event after forced abort path seen='..tostring(seen).
 				global_State* g = G(L);
 				J->state = LJ_TRACE_RECORD;
 				lj_dispatch_update(g);
-				lj_trace_abort(g);
+				J->state = static_cast<TraceState>(static_cast<unsigned>(J->state) & ~static_cast<unsigned>(LJ_TRACE_ACTIVE));
 				if (J->state != LJ_TRACE_IDLE) J->state = LJ_TRACE_IDLE;
 			}
 #endif
