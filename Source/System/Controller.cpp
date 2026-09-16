9f67b773668c072f20b44d8ebe96e0414b7d444d
Erol Germain-Gomuc <egermain.ua@gmail.com>
2026-09-16 08:46:41 -0700

    Skip the live-menu input check until MenuMan exists

--- Source/System/Controller.cpp
+++ Source/System/Controller.cpp
@@ -351,7 +351,7 @@ void Controller::Update() {
 }
 
 void Controller::RenderUpdate() {
-	if (m_Disabled || m_SeatMode != InputMode::CIM_PLAYER || GetInputPlayer() < 0 || g_MenuMan.IsLiveMenuOwningInput()) {
+	if (m_Disabled || m_SeatMode != InputMode::CIM_PLAYER || GetInputPlayer() < 0 || (MenuMan::IsConstructed() && g_MenuMan.IsLiveMenuOwningInput())) {
 		return;
 	}
 	if (m_ControlledActor && (m_ControlledActor->GetHealth() == 0.0f || m_ControlledActor->GetStatus() == Actor::DYING || m_ControlledActor->GetStatus() == Actor::DEAD)) {
@@ -438,7 +438,7 @@ void Controller::GetInputFromPlayer() {
 	std::array<bool, ControlState::CONTROLSTATECOUNT> lastControlStates = m_ControlStates;
 	ResetCommandState();
 
-	if ((g_ConsoleMan.IsEnabled() && !g_ConsoleMan.IsReadOnly()) || GetInputPlayer() < 0 || g_MenuMan.IsLiveMenuOwningInput()) {
+	if ((g_ConsoleMan.IsEnabled() && !g_ConsoleMan.IsReadOnly()) || GetInputPlayer() < 0 || (MenuMan::IsConstructed() && g_MenuMan.IsLiveMenuOwningInput())) {
 		return;
 	}
 
