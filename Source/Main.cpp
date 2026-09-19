bdec71ed8d5170dbda720f9eb8b1ef6a976af8bc
Erol Germain-Gomuc <egermain.ua@gmail.com>
2026-09-19 12:03:22 -0700

    Carry the local script message count in the boundary readout

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -6395,7 +6395,8 @@ std::string BuildControllerBoundaryJson() {
 	const MovableMan::ControllerBoundaryStats& stats = g_MovableMan.GetControllerBoundaryStats();
 	std::ostringstream out;
 	out << "{\"equip_commands\":" << stats.equipCommands << ",\"sound_commands\":" << stats.soundCommands << ",\"aim_intents\":" << stats.aimIntents
-	    << ",\"flip_intents\":" << stats.flipIntents << ",\"direct_writes\":" << stats.directWrites << "}";
+	    << ",\"flip_intents\":" << stats.flipIntents << ",\"direct_writes\":" << stats.directWrites
+	    << ",\"local_script_messages\":" << stats.localScriptMessages << "}";
 	return out.str();
 }
 
